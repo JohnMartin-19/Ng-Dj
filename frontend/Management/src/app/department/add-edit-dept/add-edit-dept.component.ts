2533dc04bf392302c527d1f659bd27f854d84cd0
John <johnnkonge2020@gmail.com>
2024-04-15 19:29:44 +0300

    updated the necessary components and services generated

---
+++ frontend/Management/src/app/department/add-edit-dept/add-edit-dept.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-dept',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-edit-dept.component.html',
+  styleUrl: './add-edit-dept.component.css'
+})
+export class AddEditDeptComponent {
+
+}
---
+++ frontend/Management/src/app/department/show-dept/show-dept.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-dept',
+  standalone: true,
+  imports: [],
+  templateUrl: './show-dept.component.html',
+  styleUrl: './show-dept.component.css'
+})
+export class ShowDeptComponent {
+
+}
