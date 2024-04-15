77473b0985e3062bb8b2e2b0cbe80abb2245b946
John <johnnkonge2020@gmail.com>
2024-04-15 15:31:52 +0300

    employee endpoints up

--- Employee/urls.py
+++ Employee/urls.py
@@ -1,9 +1,12 @@
 
 from django.urls import path
-from .views import departmentApi
+from .views import departmentApi,employeeApi
 
 
 urlpatterns = [
     path('departments/', departmentApi, name='department_list'),
     path('departments/<int:id>/', departmentApi, name='department_detail'),
+    path('employees/', employeeApi, name='employee_list'),
+    path('employees/<int:id>/', employeeApi, name='employee_detail'),
+
 ]
