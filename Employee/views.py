efee23c63ff1bc0fe8758ad6794bbc2aa0193365
John <johnnkonge2020@gmail.com>
2024-04-15 17:43:52 +0300

    endpoints working just fine

--- Employee/views.py
+++ Employee/views.py
@@ -51,14 +51,17 @@ def employeeApi(request,id=0):
         return JsonResponse('Failed to Add',safe=False)
     
     elif request.method == 'PUT':
-        employee_data = JSONParser.parse(request)
+        employee_data = JSONParser().parse(request)
+        if 'EmployeeId' not in employee_data:
+            return JsonResponse('EmployeeId is required', safe=False, status=400)
         employee = Employee.objects.get(EmployeeId=employee_data['EmployeeId'])
         employee_serializer = EmployeeSerializer(employee,data=employee_data)
         if employee_serializer.is_valid():
             employee_serializer.save()
             return JsonResponse('Updated successfully!',safe=False)
         return JsonResponse('Failed to update',safe=False)
+    
     elif request.method =='DELETE':
         employee = Employee.objects.get(EmployeeId=id)
         employee.delete()
-        return JsonResponse('Deleted Successfully')  
+    return JsonResponse('Deleted Successfully',safe=False)  
