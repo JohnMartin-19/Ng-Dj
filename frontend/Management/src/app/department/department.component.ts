ef1a1711550148992902e8cad6923042804ebf37
John <johnnkonge2020@gmail.com>
2024-04-16 17:18:07 +0300

    added display table for departments

--- frontend/Management/src/app/department/department.component.ts
+++ frontend/Management/src/app/department/department.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 
 @Component({
   selector: 'app-department',
   templateUrl: './department.component.html',
+  standalone:true,
+  schemas:[
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
   styleUrl: './department.component.css'
 })
 export class DepartmentComponent {
--- frontend/Management/src/app/department/show-dept/show-dept.component.ts
+++ frontend/Management/src/app/department/show-dept/show-dept.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { SharedService } from '../../shared.service';
 @Component({
   selector: 'app-show-dept',
   templateUrl: './show-dept.component.html',
   styleUrl: './show-dept.component.css'
 })
 export class ShowDeptComponent {
+  constructor(private service:SharedService) {}
+
+  DepartmentList: any =[]
+
+  ngOnInit(): void{
+    this.refreshDeptList()
+  }
+
+  refreshDeptList(){
+    this.service.getDeptList().subscribe(data => {this.DepartmentList= data});
+  }
 
 }
