92b69d2ccef828538f28d065aefd63cbe2cdf709
John <johnnkonge2020@gmail.com>
2024-04-18 10:48:36 +0300

    fixing bugs

--- frontend/Management/src/app/employee/show-emp/show-emp.component.ts
+++ frontend/Management/src/app/employee/show-emp/show-emp.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { SharedService } from '../../shared.service';
 @Component({
   selector: 'app-show-emp',
   templateUrl: './show-emp.component.html',
@@ -7,4 +7,58 @@ import { Component } from '@angular/core';
 })
 export class ShowEmpComponent {
 
+  constructor(private service:SharedService) {}
+
+  EmployeeList: any =[];
+
+  ModalTitle:string | undefined;
+  ActivateAddEditEmpComp:boolean=false
+  emp:any;
+
+
+  ngOnInit(): void{
+    this.refreshEmpList()
+  }
+
+  addClick(){
+    this.emp={
+      EmployeeId :0,
+      EmployeeName :'',
+      Department :'',
+      DateofJoining:'',
+      PhotoFileName:'anonymus.png',
+
+    }
+    this.ModalTitle = 'Add Employee'
+    this.ActivateAddEditEmpComp=true
+  }
+
+  closeClick(){
+   this.ActivateAddEditEmpComp=false
+   this.refreshEmpList();
+  }
+
+
+  editClick(item: any){
+    this.emp = item
+    this.ModalTitle='Edit Employee.'
+    this.ActivateAddEditEmpComp=true
+
+  }
+
+  deleteClick(item:any){
+    if(confirm('Are you sure you want to delete??')){
+      this.service.deleteEmp(item.EmployeeId).subscribe(data=> {
+        alert(data.toString)
+        this.refreshDeptList()
+      })
+    }
+  }
+
+  refreshEmpList(){
+    this.service.getDeptList().subscribe(data => {this.EmployeeList= data});
+  }
+
 }
+
+
